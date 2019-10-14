000674b4b4e70982217900086ecc7bb76ce957e9
Carla Holcomb <carla.programmer@gmail.com>
2019-10-14 02:27:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+  if array.size == 2
+array.join(" and ")
+elsif array.size > 2
+array[0..-2].join(", ") + ", and " + array[-1]
+else array.size == 1
+  array.join(" , ")
 
+  end
 end
